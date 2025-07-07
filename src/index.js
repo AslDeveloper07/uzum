32dd62c8ce6e547e688c63f0e2caf04beff26f7a
AslDeveloper07 <aslwexking777@gmail.com>
2025-07-07 13:28:10 +0500

    sadsa

--- src/index.js
+++ src/index.js
@@ -6,128 +6,128 @@ const API = [
   {
     id: 1,
     image: "https://images.uzum.uz/d1jntsi1146hm2be98d0/t_product_540_high.jpg",
-    price: 2314321,
+    price: '23 143 21',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 2,
     image: "https://images.uzum.uz/d16lnb33uvppu2abq550/t_product_540_high.jpg",
-    price: 23321141,
+    price: '23 321 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 3,
     image: "https://images.uzum.uz/curci9bvgbkm5ehfsslg/t_product_540_high.jpg",
-    price: 2313241,
+    price: '23 132 41',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 4,
     image: "https://images.uzum.uz/d0e5h9q7s4fo7mq8ahqg/t_product_540_high.jpg",
-    price: 2314321,
+    price: '23 143 21',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 5,
     image: "https://images.uzum.uz/d0tfetr3uvph50a0lssg/t_product_540_high.jpg",
-    price: 2314321,
+    price: '23 143 21',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 6,
     image: "https://images.uzum.uz/ctrs906i4n324lr25hig/t_product_540_high.jpg",
-    price: 231324441,
+    price: '23 132 4441',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
   },
   {
     id: 7,
     image: "https://images.uzum.uz/d03jtslpb7f46s87tvc0/t_product_540_high.jpg",
-    price: 2314341,
+    price: '23 143 41',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 8,
     image: "https://images.uzum.uz/ctrs906i4n324lr25hig/t_product_540_high.jpg",
-    price: 2523141,
+    price: '25 231 41',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 9,
     image: "https://images.uzum.uz/d1akrs33uvppu2acni9g/t_product_540_high.jpg",
-    price: 25323141,
+    price: '25 323 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
   },
   {
     id: 10,
     image: "https://images.uzum.uz/d15bt98n274lpu394m80/t_product_540_high.jpg",
-    price: 25433141,
+    price: '25 433 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 11,
     image: "https://images.uzum.uz/d1hrq7eojia34bc0020g/t_product_540_high.jpg",
-    price: 52323141,
+    price: '52 323 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
   },
   {
     id: 12,
     image: "https://images.uzum.uz/d0vbavr3uvpglcmae6e0/t_product_540_high.jpg",
-    price: 54323141,
+    price: '54 323 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 13,
     image: "https://images.uzum.uz/d15bt98n274lpu394m80/t_product_540_high.jpg",
-    price: 53423141,
+    price: '53 423 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
   },
   {
     id: 14,
     image: "https://images.uzum.uz/ctc636ui4n3ehka3cr4g/t_product_540_high.jpg",
-    price: 54323141,
+    price: '54 323 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 15,
     image: "https://images.uzum.uz/cupphetht56sc95ffng0/t_product_540_high.jpg",
-    price: 54323141,
+    price: '54 323 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
   },
   {
     id: 16,
     image:
       "https://images.uzum.uz/ctrs906i4n324lr25hig/t_product_540_high.jpg",
-    price: 54323141,
+    price: '54 323 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 17,
     image: "https://images.uzum.uz/d1h0pvql822nnunhlb50/t_product_540_high.jpg",
-    price: 23141,
+    price: '23 141 ',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
   {
     id: 18,
     image: "https://images.uzum.uz/d08v4iuf4hvqhbr48aj0/original.jpg",
-    price: 23141,
+    price: '23 141 ',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
   },
   {
     id: 19,
     image: "https://images.uzum.uz/d03onec7fd1idpht7sgg/original.jpg",
-    price: 23147651,
+    price: '23 147 651',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
   },
   {
@@ -140,26 +140,42 @@ const API = [
   {
     id: 21,
     image: "https://images.uzum.uz/d17r7f0n274lpu39l0h0/t_product_540_high.jpg",
-    price: 2354141,
+    price: '23 541 41',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
   },
   {
     id: 22,
     image: "https://images.uzum.uz/d19vhp33uvppu2aci4mg/t_product_540_high.jpg",
-    price: 231543541,
+    price: '23 154 354',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    stars: `14 (37.9999)`,
+  },
+    {
+    id: 23,
+    image: "https://images.uzum.uz/cupphetht56sc95ffng0/t_product_540_high.jpg",
+    price: '54 323 141',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  },
+  {
+    id: 24,
+    image:
+      "https://images.uzum.uz/ctrs906i4n324lr25hig/t_product_540_high.jpg",
+    price: '54 323 141',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     stars: `14 (37.9999)`,
   },
 ];
 
-const BookList = ({ id, image, description, price, stars }) => {
+const BookList = ({ id, image, description, price, stars,  }) => {
   return (
     <section className="bookList">
-      <img src={image} alt={description} />
+      <div className="imege_scale">
+        <img src={image} alt={description} />
+      </div>
       <div className="text">
         <h2>{price}</h2>
         <p className="about">{description}</p>
-        <p>{stars}</p>
+        <p>{stars ? stars : 'News'}</p>
         <button className="btn">Add to Card</button>
       </div>
     </section>
